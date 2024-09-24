8d9a8d3dfa529885b120414a9692d42d94b6060c
ivanvn19 <ivan.vazquezneira@gmail.com>
2024-09-24 15:55:16 -0300

    JFK

---
+++ data.js
@@ -0,0 +1,589 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frente",
+      "name": "FRENTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.05412928277792162,
+        "pitch": 0.14717399097386163,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2563687978400147,
+          "pitch": 0.064855959895052,
+          "rotation": 0.7853981633974483,
+          "target": "2-patio-ext"
+        },
+        {
+          "yaw": 0.7664396656555041,
+          "pitch": 0.14016883648216094,
+          "rotation": 0,
+          "target": "1-garage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-garage",
+      "name": "GARAGE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5794109367958704,
+          "pitch": 0.057835649037144776,
+          "rotation": 0,
+          "target": "3-ingreso"
+        },
+        {
+          "yaw": -3.0281968548112985,
+          "pitch": 0.30284267761920347,
+          "rotation": 0,
+          "target": "0-frente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-patio-ext",
+      "name": "PATIO EXT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3997500488710024,
+          "pitch": 0.14956159596172114,
+          "rotation": 0,
+          "target": "3-ingreso"
+        },
+        {
+          "yaw": -2.312592867733514,
+          "pitch": 0.13878446970738167,
+          "rotation": 0,
+          "target": "0-frente"
+        },
+        {
+          "yaw": 2.472072893497728,
+          "pitch": 0.1465652899032044,
+          "rotation": 0,
+          "target": "0-frente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ingreso",
+      "name": "INGRESO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0696428123021757,
+          "pitch": 0.1472051131682619,
+          "rotation": 0,
+          "target": "2-patio-ext"
+        },
+        {
+          "yaw": -0.04484664593527832,
+          "pitch": 0.044372226260918524,
+          "rotation": 0,
+          "target": "4-living---comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-living---comedor",
+      "name": "LIVING - COMEDOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7608063842351456,
+          "pitch": 0.1540151798818492,
+          "rotation": 0,
+          "target": "3-ingreso"
+        },
+        {
+          "yaw": -1.433717488402003,
+          "pitch": 0.13231509689705945,
+          "rotation": 0,
+          "target": "5-cocina"
+        },
+        {
+          "yaw": -0.8237001056510032,
+          "pitch": 0.15558702938316493,
+          "rotation": 0,
+          "target": "6-patio"
+        },
+        {
+          "yaw": -2.4234204854091175,
+          "pitch": 0.19182864016816836,
+          "rotation": 0,
+          "target": "8-ingreso-bao---habitaciones-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-cocina",
+      "name": "COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.42679124149039893,
+        "pitch": 0.2462911685685203,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2525221908062267,
+          "pitch": 0.1942644773476907,
+          "rotation": 0,
+          "target": "4-living---comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-patio",
+      "name": "PATIO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1232335841128,
+          "pitch": 0.24036765627349865,
+          "rotation": 0,
+          "target": "4-living---comedor"
+        },
+        {
+          "yaw": -0.9353665482889273,
+          "pitch": 0.11924559009616154,
+          "rotation": 11.780972450961727,
+          "target": "7-patio-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-patio-ii",
+      "name": "PATIO II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.831606903937594,
+          "pitch": 0.13576882702863458,
+          "rotation": 1.5707963267948966,
+          "target": "6-patio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-ingreso-bao---habitaciones-",
+      "name": "INGRESO BAÑO - HABITACIONES ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03449239306325147,
+          "pitch": 0.10544739782706536,
+          "rotation": 0,
+          "target": "9-ingreso-bao---habitaciones-ii"
+        },
+        {
+          "yaw": 1.7189246947891332,
+          "pitch": 0.12649218130645323,
+          "rotation": 0,
+          "target": "10-bao"
+        },
+        {
+          "yaw": -3.047787489644662,
+          "pitch": 0.17839535297539477,
+          "rotation": 0,
+          "target": "4-living---comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-ingreso-bao---habitaciones-ii",
+      "name": "INGRESO BAÑO - HABITACIONES II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5690140805988797,
+          "pitch": 0.018172606474307074,
+          "rotation": 0,
+          "target": "11-habitacin-principal"
+        },
+        {
+          "yaw": 1.5167421170700361,
+          "pitch": 0.04469804373578512,
+          "rotation": 0,
+          "target": "12-habitacin"
+        },
+        {
+          "yaw": 2.7195181565281343,
+          "pitch": 0.14700013574428183,
+          "rotation": 5.497787143782138,
+          "target": "10-bao"
+        },
+        {
+          "yaw": 3.1179198535935893,
+          "pitch": 0.12842377919037062,
+          "rotation": 0,
+          "target": "4-living---comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-bao",
+      "name": "BAÑO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.2125287034184176,
+        "pitch": 0.25052249573666785,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0655319648540615,
+          "pitch": 0.1585377412153406,
+          "rotation": 0,
+          "target": "8-ingreso-bao---habitaciones-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-habitacin-principal",
+      "name": "HABITACIÓN PRINCIPAL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.08561263055655388,
+        "pitch": 0.35123097860296326,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.870954778633889,
+          "pitch": 0.16005432767584615,
+          "rotation": 0,
+          "target": "9-ingreso-bao---habitaciones-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-habitacin",
+      "name": "HABITACIÓN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3848139156973325,
+          "pitch": 0.07449237210355619,
+          "rotation": 0,
+          "target": "9-ingreso-bao---habitaciones-ii"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CASA JF. KENEDY TOUR ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
